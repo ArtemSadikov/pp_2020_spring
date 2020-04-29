e290b0897f6d051d895f53a339eee608a2e0174d
ArtemSadikov <marolboron@ya.ru>
2020-04-29 22:55:23 +0300

    pls_4

--- modules/task_3/sadikov_a_deikstra_algorithm/main.cpp
+++ modules/task_3/sadikov_a_deikstra_algorithm/main.cpp
@@ -7,10 +7,7 @@
 
 TEST(Deikstra_Algorithm_TBB, Test_On_Large_Graph) {
     std::vector<int> act = getRandGraph(10);
-    // double st = clock();
     std::vector<int> res = getMinRange(act, 1, 10);
-    // double end = (double)(clock() - st) / CLOCKS_PER_SEC;
-    // std::cout << end;
     ASSERT_EQ(1, 1);
 }
 
