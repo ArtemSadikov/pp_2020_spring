cade8adc8bd88c7d505bae001fdeddeecba774be
ArtemSadikov <marolboron@ya.ru>
2020-04-30 00:21:54 +0300

    ...

--- modules/task_3/sadikov_a_deikstra_algorithm/main.cpp
+++ modules/task_3/sadikov_a_deikstra_algorithm/main.cpp
@@ -5,13 +5,13 @@
 #include <ctime>
 #include "./deikstra_algorithm.h"
 
-// TEST(Deikstra_Algorithm_TBB, Test_On_Large_Graph) {
-//    std::vector<int> act = getRandGraph(10);
-//    std::vector<int> res = getMinRange(act, 1, 10);
-//    ASSERT_EQ(1, 1);
-// }
+TEST(Deikstra_Algorithm_TBB, Test_On_Large_Graph) {
+    std::vector<int> act = getRandGraph(1000);
+    std::vector<int> res = getMinRange(act, 1, 554);
+    ASSERT_EQ(1, 1);
+}
 
-TEST(DISAPLED_Deikstra_Algorithm_TBB, Test_On_Six_Points_Graph) {
+TEST(Deikstra_Algorithm_TBB, Test_On_Six_Points_Graph) {
     std::vector<int> g = {0, 7, 9, 0, 0, 14,
                           7, 0, 10, 15, 0, 0,
                           9, 10, 0, 11, 0, 2,
@@ -53,7 +53,7 @@ TEST(Deikstra_Algorithm_TBB, Test_Return_0_If_Equal_Start_And_End) {
     EXPECT_EQ(getMinRange(g, 3, 3), std::vector<int>(1, 0));
 }
 
-TEST(DISABLED_Deikstra_Algorithm_TBB, Test_On_Six_Points_Graph_Another_Way) {
+TEST(Deikstra_Algorithm_TBB, Test_On_Six_Points_Graph_Another_Way) {
     std::vector<int> g = {0, 7, 9, 0, 0, 14,
                           7, 0, 10, 15, 0, 0,
                           9, 10, 0, 11, 0, 2,
